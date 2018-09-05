7c6c56e4a6501c5b4dcfc2e33c898c3eb8d9f1ed
liberot <vberzsin@web.de>
2018-09-06 00:43:09 +0200

    kozmetikc

--- www/server.4.js
+++ www/server.4.js
@@ -17,41 +17,40 @@ const proxy = http.createServer((req, res1) => {
       var buf = '';
         
       https.get(targetUrl, function(res2) {
-      	
-      	res2.on('data', function(data) {
-      		buf += data;
-      	});
-   	        	
-      	res2.on('end', function() {
-      			
-      			buf = buf.replace(/<head>(?:.|\n|\r)+?<\/head>/, '');
-      			buf = buf.replace(/<script>(?:.|\n|\r)+?<\/script>/, '');
-      			buf = buf.replace(/<(?:.|\n)*?>/gm, '');
-            buf = buf.replace(/\n\s*\n/g, '\n');
-            buf = buf.replace(/\s\s+/g, ' ');
 
-            var xml = '<?xml version="1.0" encoding="utf-8"?>';
-                xml+= '<result>' +buf +'</result>';
-            // 
-            var fn = targetUrl.replace(/\//ig, ':'); 
-                fn = fn.replace(/:/ig, ':'); 	
-              	fn = './dload/' +fn;
-
-            fs.writeFile(fn, xml, function(err) {
-            	if(err){
-            		console.log('could not write file: ' +fn);
-            	}
-            	else {
-            		console.log('written to: ' +fn);
-            	}
-            });
-  			       			      	
-      		  res1.writeHead(200, {'Content-Type': 'application/xml'});
-   	        res1.end(xml);
-          		
-            return;
+        res2.on('data', function(data) {
+          buf += data;
         });
-   	  });
+        
+        res2.on('end', function() {
+          buf = buf.replace(/<head>(?:.|\n|\r)+?<\/head>/, '');
+          buf = buf.replace(/<script>(?:.|\n|\r)+?<\/script>/, '');
+          buf = buf.replace(/<(?:.|\n)*?>/gm, '');
+          buf = buf.replace(/\n\s*\n/g, '\n');
+          buf = buf.replace(/\s\s+/g, ' ');
+
+          var xml = '<?xml version="1.0" encoding="utf-8"?>';
+              xml+= '<result>' +buf +'</result>';
+          // 
+          var fn = targetUrl.replace(/\//ig, ':'); 
+              fn = fn.replace(/:/ig, ':');
+              fn = './dload/' +fn;
+
+          fs.writeFile(fn, xml, function(err) {
+            if(err){
+              console.log('could not write file: ' +fn);
+            }
+            else {
+              console.log('written to: ' +fn);
+            }
+          });
+       
+          res1.writeHead(200, {'Content-Type': 'application/xml'});
+            res1.end(xml);
+        
+          return;
+        });
+      });
     });  
     return;
 
@@ -59,7 +58,7 @@ const proxy = http.createServer((req, res1) => {
 
   var doc = fs.readFileSync('reader.html');
   res1.writeHead(200, {'Content-Type': 'text/html'});
-	res1.end(doc);
+  res1.end(doc);
 
 });
 
